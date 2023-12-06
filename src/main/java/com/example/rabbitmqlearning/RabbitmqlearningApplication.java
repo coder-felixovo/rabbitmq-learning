936902a8860751c35d6bac9a250ecb65cb63e015
felixovo <felix2022daily@qq.com>
2023-12-06 18:01:46 +0800

    Initial project

---
+++ src/main/java/com/example/rabbitmqlearning/RabbitmqlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqlearningApplication.class, args);
+    }
+
+}
